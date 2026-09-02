dd606660ce28bbf5a126d466d349718ec8077873
João Cercal <jpcercal@gmail.com>
2026-09-02 10:58:48 +0100

    chore(scripts): drop pyenv from support-source-path helper

--- scripts/support-source-path.sh
+++ scripts/support-source-path.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export PATH="/opt/homebrew/bin:~/dotfiles/bin:$PATH"
-export PATH="$(brew --prefix ruby)/bin:$(ruby -e 'puts Gem.bindir'):$PATH"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$HOME/.local/bin:/opt/homebrew/bin:~/dotfiles/bin:$PATH"
+export PATH="$HOMEBREW_OPT/ruby/bin:$GEM_BINDIR:$PATH"
 export PATH="$GOPATH/bin:$PATH"
-export PATH="$HOME/.serverless/bin:$PATH"
